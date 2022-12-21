e7df97e4192ff861fe4dff4a243a2abe5e25f5fb
Brian Salkas <brian@pop-os.localdomain>
2022-12-20 18:34:42 -0600

    init commit

---
+++ ddl.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS organism (
+	genus VARCHAR(40) NOT NULL,
+	species VARCHAR(40) NOT NULL,
+	kingdom VARCHAR(15),
+	habitat VARCHAR(30),
+	conservation_status VARCHAR(30) CHECK (conservation_status IN ('GX','GH','G5','G4','G3','G2','G1')),
+	population INT,
+	CONSTRAINT PK_Species PRIMARY KEY (genus, species)
+);
+
+-- How do I set this up?  
+-- should I enforce that a spieces exists in the organisms table
+-- should I create some kind of primary key?
+CREATE TABLE IF NOT EXISTS foodchain (
+	prey_genus VARCHAR(40),
+	prey_species VARCHAR(40),
+	preditor_genus VARCHAR(40),
+	preditor_species VARCHAR(40),
+	CONSTRAINT FK_Preditor FOREIGN KEY (preditor_genus,preditor_species) REFERENCES organism(genus,species),
+	CONSTRAINT FK_Prey FOREIGN KEY (prey_genus, prey_species) REFERENCES organism(genus,species)
+);
+
+
+
---
+++ dml.sql
@@ -0,0 +1,29 @@
+INSERT INTO foodchain (
+	eaten_species,
+	eaten_genus,
+	eats_speices,
+	eats_genus
+)
+VALUES (
+	'macrochirus',
+	'lepomis',
+	'salmoides',
+	'micropterus'
+)
+
+INSERT INTO organism (
+	species,
+	genus,
+	kingdom,
+	habitat,
+	conservation_status,
+	population
+)
+VALUES (
+	'small',
+	'fish',
+	'animal',
+	'fresh water',
+	'G5',
+	1000000
+);
